a80226d227f786640431defa60cca5fda9b6330f
Zgroza Klimek <zgroza.klimek@gmail.com>
2026-05-30 20:58:48 +0200

    Hypr: better window rules

--- hypr/lua/00-variables.lua
+++ hypr/lua/00-variables.lua
@@ -4,7 +4,4 @@ M.mainMod = "SUPER"
 M.term = "kitty"
 M.launcher = 'zsh -i -c "pkill rofi || rofi -show combi"'
 
-hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
-hl.env("GTK_THEME", "adw-gtk3-dark")
-
 return M
--- hypr/lua/05-window-rules.lua
+++ hypr/lua/05-window-rules.lua
@@ -1,8 +1,33 @@
-hl.window_rule({ match = { title = "^WPRS.*$" }, float = true })
-hl.window_rule({ match = { title = "^nmtui$" }, float = true })
-hl.window_rule({ match = { class = "^org.pulseaudio.pavucontrol$" }, float = true })
-hl.window_rule({ match = { class = "^nm-connection-editor$" }, float = true })
-hl.window_rule({ match = { class = "^blueman-manager$" }, float = true })
-hl.window_rule({ match = { class = "^xdg-desktop-portal-gtk$" }, float = true })
-hl.window_rule({ match = { title = "^Picture in picture$" }, float = true, pin = true })
-hl.window_rule({ match = { class = "^google-chrome-beta-glic$" }, float = true })
+local function extend(base, override)
+  local result = {}
+  for k, v in pairs(base or {}) do result[k] = v end
+  for k, v in pairs(override or {}) do result[k] = v end
+  return result
+end
+
+local common_opts = {
+  float = true,
+  center = true,
+  size = {
+    "monitor_w * 0.7",
+    "monitor_h * 0.7",
+  },
+}
+
+hl.window_rule(extend(common_opts, { match = { title = "^nmtui$", }, }))
+hl.window_rule(extend(common_opts, { match = { class = "^org.pulseaudio.pavucontrol$", }, }))
+hl.window_rule(extend(common_opts, { match = { class = "^blueman-manager$", }, }))
+hl.window_rule(extend(common_opts, { match = { class = "^nm-connection-editor$", }, }))
+hl.window_rule({
+  match = { title = "^Unlock Database - KeePassXC$", },
+  float = true,
+  stay_focused = true,
+})
+hl.window_rule({ match = { title = "^WPRS.*$", }, float = true, })
+hl.window_rule({
+  match = { class = "^xdg-desktop-portal-gtk$", },
+  float = true,
+  center = true,
+  stay_focused = true,
+})
+hl.window_rule({ match = { title = "^Picture in picture$", }, float = true, pin = true, })

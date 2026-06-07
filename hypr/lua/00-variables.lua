81352a66f60198477dc99f6106b4ce0d8594486d
Zgroza Klimek <zgroza.klimek@gmail.com>
2026-06-07 12:30:29 +0200

    Hypr: some QT adjustments

--- hypr/lua/00-variables.lua
+++ hypr/lua/00-variables.lua
@@ -4,4 +4,9 @@ M.mainMod = "SUPER"
 M.term = "kitty"
 M.launcher = 'zsh -i -c "pkill rofi || rofi -show combi"'
 
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+hl.env("QT_WAYLAND_DISABLE_WINDOWDECORATION", "1")
+hl.env("QT_QPA_PLATFORM", "wayland")
+hl.env("QT_AUTO_SCREEN_SCALE_FACTOR", "1")
+
 return M

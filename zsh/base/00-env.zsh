6bf4df11983207064e3a9b6b50cae87da82a1d35
Zgroza Klimek <zgroza@chromium.org>
2026-06-24 11:58:37 +0200

    nvm

--- zsh/base/00-env.zsh
+++ zsh/base/00-env.zsh
@@ -10,3 +10,6 @@ export MANPAGER='nvim +Man!'
 
 export SSH_AUTH_SOCK="${SSH_AUTH_SOCK:-$XDG_RUNTIME_DIR/ssh-agent.socket}"
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
